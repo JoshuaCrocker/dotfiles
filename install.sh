019c1badfebca0df71c44178459d1bc239cce0a5
Joshua Crocker <JoshuaCrocker@users.noreply.github.com>
2023-04-06 14:49:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,6 +62,13 @@ else
         exit
     fi
 fi
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 }
 
 install_zsh
